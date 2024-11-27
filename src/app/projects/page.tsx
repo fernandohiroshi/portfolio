18d4bee2d0bd8a579ba953e4c7282f47e38f78bb
fernandohiroshi <contatofernandohiroshitakeda@outlook.com>
2024-11-27 19:24:30 -0300

    feat: create projects page

---
+++ src/app/projects/page.tsx
@@ -0,0 +1,12 @@
+import Project from '@/components/view/project/Project'
+
+export default function ProjectPage() {
+  return (
+    <main className="mx-auto mb-8 min-h-screen max-w-4xl scroll-mt-24 px-4" id="projects">
+      <h1 className="mt-16 animate-pulse text-4xl uppercase text-purple-500">Projects</h1>
+      <div className="mx-auto mt-8 grid max-w-4xl grid-flow-dense grid-cols-12 gap-4">
+        <Project />
+      </div>
+    </main>
+  )
+}
