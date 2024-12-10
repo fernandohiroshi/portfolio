053ceecf094b1e68461f819657e8a9b1a6e20c01
fernandohiroshi <contatofernandohiroshitakeda@outlook.com>
2024-12-10 06:18:21 -0300

    feat: add projects-datas.ts to separate data from Project.tsx

--- src/components/pages/project/Project.tsx
+++ src/components/pages/project/Project.tsx
@@ -4,24 +4,11 @@ import { motion } from 'framer-motion'
 import Image from 'next/image'
 import { useState } from 'react'
 
+import { projects } from '@/lib/projects-datas'
+
 import { Lens } from '@/components/ui/aceternity/lens'
 import { Button } from '@/components/ui/shadcn/button'
 import Box from '@/components/view/bento/Box'
-
-const projects = [
-  {
-    img: 'https://imgix.cosmicjs.com/b1e6b4b0-80fe-11ef-9257-112b43bd2f70-Macbook-Air-konbinicode.com.png',
-    name: 'Konbini Code',
-    code: 'https://github.com/fernandohiroshi/konbini-code',
-    live: 'https://konbinicode.com/',
-  },
-  {
-    img: 'https://imgix.cosmicjs.com/c07f4730-80fe-11ef-9257-112b43bd2f70-Macbook-Air-fernandohiroshi.com.png',
-    name: 'My Portfolio',
-    code: 'https://github.com/fernandohiroshi/portfolio',
-    live: 'https://fernandohiroshi.com/',
-  },
-]
 
 const Project = () => {
   const [hoveringIndex, setHoveringIndex] = useState<number | null>(null)
@@ -29,7 +16,7 @@ const Project = () => {
   return (
     <>
       {projects.map((p, index) => (
-        <Box key={index} className="col-span-12 flex flex-col justify-evenly md:col-span-6">
+        <Box key={index} className="col-span-12 flex flex-col justify-evenly p-2 md:col-span-6 md:p-4">
           <Lens
             hovering={hoveringIndex === index}
             setHovering={(isHovering) => setHoveringIndex(isHovering ? index : null)}
@@ -51,16 +38,20 @@ const Project = () => {
             <div className="flex items-center justify-between">
               <h2 className="text-base md:text-2xl">{p.name}</h2>
               <div className="flex items-center gap-4">
-                <a href={p.code}>
-                  <Button variant="outline" size="sm">
-                    Code
-                  </Button>
-                </a>
-                <a href={p.live}>
-                  <Button variant="outline" size="sm">
-                    Live
-                  </Button>
-                </a>
+                {p.codeBtn && (
+                  <a href={p.code}>
+                    <Button variant="outline" size="sm">
+                      Code
+                    </Button>
+                  </a>
+                )}
+                {p.liveBtn && (
+                  <a href={p.live}>
+                    <Button variant="outline" size="sm">
+                      Live
+                    </Button>
+                  </a>
+                )}
               </div>
             </div>
           </motion.div>
---
+++ src/lib/projects-datas.ts
@@ -0,0 +1,34 @@
+export const projects = [
+  {
+    img: 'https://imgix.cosmicjs.com/b1e6b4b0-80fe-11ef-9257-112b43bd2f70-Macbook-Air-konbinicode.com.png',
+    name: 'Konbini Code',
+    code: 'https://github.com/fernandohiroshi/konbini-code',
+    live: 'https://konbinicode.com/',
+    codeBtn: true,
+    liveBtn: true,
+  },
+  {
+    img: 'https://imgix.cosmicjs.com/c07f4730-80fe-11ef-9257-112b43bd2f70-Macbook-Air-fernandohiroshi.com.png',
+    name: 'My Portfolio',
+    code: 'https://github.com/fernandohiroshi/portfolio',
+    live: 'https://fernandohiroshi.com/',
+    codeBtn: true,
+    liveBtn: true,
+  },
+  {
+    img: 'https://imgix.cosmicjs.com/97b5d5e0-ae39-11ef-8a63-eb57d6c77a36-profilesite.png',
+    name: 'carolinesouza.art',
+    code: 'https://github.com/fernandohiroshi/carolinesouza',
+    live: 'https://www.carolinesouza.art/',
+    codeBtn: true,
+    liveBtn: true,
+  },
+  {
+    img: 'https://imgix.cosmicjs.com/a7fc4020-b202-11ef-8a63-eb57d6c77a36-top-bebidas.png',
+    name: 'topbebidas.com',
+    code: '',
+    live: 'https://topbebidas.com/',
+    codeBtn: false,
+    liveBtn: true,
+  },
+]
